180c11be83cbfbb154062d8bc7ced6373db337ca
Doy Kim <kimdee.dev@gmail.com>
2022-11-09 08:55:50 +0900

    Initial Commit

---
+++ MogakStudyUIFramework/MogakStudyUIFramework.h
@@ -0,0 +1,18 @@
+//
+//  MogakStudyUIFramework.h
+//  MogakStudyUIFramework
+//
+//  Created by Doy Kim on 2022/11/09.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for MogakStudyUIFramework.
+FOUNDATION_EXPORT double MogakStudyUIFrameworkVersionNumber;
+
+//! Project version string for MogakStudyUIFramework.
+FOUNDATION_EXPORT const unsigned char MogakStudyUIFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MogakStudyUIFramework/PublicHeader.h>
+
+
