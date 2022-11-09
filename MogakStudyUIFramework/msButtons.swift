3418c00cefa727da40a3db0c97afe23881f9bcf5
Doy Kim <kimdee.dev@gmail.com>
2022-11-09 09:22:23 +0900

    [Design] #1 버튼 상태별, 종류별, 아이콘별 열거형으로 선언

--- MogakStudyUIFramework/msButtons.swift
+++ MogakStudyUIFramework/msButtons.swift
@@ -5,4 +5,23 @@
 //  Created by Doy Kim on 2022/11/09.
 //
 
-import Foundation
+import UIKit
+
+enum ButtonStatus {
+    case inactive
+    case fill
+    case outline
+    case cancel
+    case disable
+}
+
+enum ButtonHeight: Int {
+    case h48 = 48
+    case h40 = 40
+    case h32_Icon = 32
+}
+
+enum ButtonIcon {
+    case noIcon
+    case xIcon
+}
