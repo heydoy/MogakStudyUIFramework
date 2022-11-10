34268cc40f3922c8572c1cece8e6fbc6f026613a
Doy Kim <kimdee.dev@gmail.com>
2022-11-10 16:42:05 +0900

    [Design] #1 커스텀 버튼 상태 열거형을 프로토콜+구조체로 변환 / 접근제어 public / 생성자로 옵션을 매개변수로 받고 배경색, 보더, 코너, 글자색 설정

--- MogakStudyUIFramework/msButtons.swift
+++ MogakStudyUIFramework/msButtons.swift
@@ -7,21 +7,72 @@
 
 import UIKit
 
-enum ButtonStatus {
-    case inactive
-    case fill
-    case outline
-    case cancel
-    case disable
+
+public protocol ButtonStatusColor {
+    var borderColor: CGColor { get }
+    var backgroundColor: UIColor { get }
+    var titleColor: UIColor { get }
+}
+public struct InactiveButton: ButtonStatusColor {
+    public init() {}
+    public var borderColor: CGColor = UIColor.msColor.gray004.cgColor
+    public var backgroundColor: UIColor = .msColor.white
+    public var titleColor: UIColor = .msColor.black
+}
+public struct FillButton: ButtonStatusColor {
+    public init() {}
+    public var borderColor: CGColor = UIColor.msColor.brandGreen.cgColor
+    public var backgroundColor: UIColor = .msColor.brandGreen
+    public var titleColor: UIColor = .white
+}
+public struct OutlineButton: ButtonStatusColor {
+    public init() {}
+    public var borderColor: CGColor = UIColor.msColor.brandGreen.cgColor
+    public var backgroundColor: UIColor = UIColor.msColor.white
+    public var titleColor: UIColor = .msColor.brandGreen
+}
+public struct CancelButton: ButtonStatusColor {
+    public init() {}
+    public var borderColor: CGColor = UIColor.msColor.gray002.cgColor
+    public var backgroundColor: UIColor = .msColor.gray002
+    public var titleColor: UIColor = .msColor.black
+}
+public struct DisableButton: ButtonStatusColor {
+    public init() {}
+    public var borderColor: CGColor = UIColor.msColor.gray006.cgColor
+    public var backgroundColor: UIColor = .msColor.gray006
+    public var titleColor: UIColor = .msColor.gray003
 }
 
-enum ButtonHeight: CGFloat {
+
+public enum ButtonHeight: CGFloat {
     case h48 = 48
     case h40 = 40
     case h32_Icon = 32
 }
 
-enum ButtonIcon {
+public enum ButtonIcon {
     case noIcon
     case xIcon
+}
+
+open class msButton: UIButton {
+    // MARK: - Initialiser
+    
+    required public init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    required public init(status: ButtonStatusColor, height: ButtonHeight, icon: ButtonIcon) {
+        super.init(frame: CGRect.zero)
+        
+        backgroundColor = status.backgroundColor
+        layer.borderWidth = 1
+        layer.borderColor = status.borderColor
+        layer.cornerRadius = 8
+        clipsToBounds = true
+        setTitleColor(status.titleColor, for: .normal)
+
+    }
+    
 }
